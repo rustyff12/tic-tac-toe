733fa06151b0828e1fd377d31e7eaec9f2a69ff7
Russell Frankpitt <rustyff123@gmail.com>
2024-03-17 00:14:24 +0100

    remove old files

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-import ComputerPlayer from "./computer-player";
-import TTT from "./ttt";
-const computerPlayer = new ComputerPlayer();
-const board = new TTT();
-const gameGridContainer = document.querySelector(".game-grid-container");
-
-gameGridContainer.addEventListener("click", (e) => {
-    console.log(e.target);
-});
